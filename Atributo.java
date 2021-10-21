3aece5105f6912fc013879791e838f5b7330c83c
Gabriela Pellin Perosa <gpp260203@gmail.com>
2021-10-21 19:28:42 +0000

    Herança aula 2

---
+++ Atributo.java
@@ -0,0 +1,6 @@
+public class Atributo{
+    private int numero;
+    public int numero2;
+           int numero3;
+    protected int numero4; //funciona em todas as pastas que estão dentro do projeto       
+}
---
+++ Programa.java
@@ -0,0 +1,19 @@
+public class Programa{
+    public static void main(String[] args){
+        /* while(true){
+            int cont = 0;
+            cont += 1;
+            if(cont>0){
+                break
+            System.out.println(cont)
+            }
+        }*/
+        int numero = 10;
+        Atributo2 at = new Atributo2();
+
+        at.numero2 = 10;
+        
+        System.out.println(at.numero2);
+
+    }
+}
